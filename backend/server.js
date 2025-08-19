b349538d8881201727056c96a788ca5627f6b3fb
prashantdesale259 <prashantdesale259@gmail.com>
2025-08-19 12:49:53 +0530

    complate

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,8 @@ app.use('/api/', limiter);
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL || 'http://localhost:3000' || 'https://deepnex-fashionex.vercel.app',
+
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
